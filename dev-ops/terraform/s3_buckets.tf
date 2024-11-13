a7254f1f026da922b8f94cba01211583013f27fe
yzhbankov <yzhbankov@redpointpositioning.com>
2024-11-13 01:06:58 +0000

    wip

--- dev-ops/terraform/s3_buckets.tf
+++ dev-ops/terraform/s3_buckets.tf
@@ -72,20 +72,33 @@ resource "aws_s3_bucket_policy" "bucket_two_public_access_policy" {
   })
 }
 
+resource "aws_s3_bucket_public_access_block" "static_website_one" {
+  bucket                  = aws_s3_bucket.static_website_one.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_public_access_block" "static_website_two" {
+  bucket                  = aws_s3_bucket.static_website_two.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 # Upload files to bucket one
 resource "aws_s3_object" "bucket_one_files" {
   for_each = { for file in local.website_one_files : file => file }
   bucket   = aws_s3_bucket.static_website_one.bucket
   key      = each.value
   source   = "${path.module}/../../apps/web-site-one/${each.value}"
-  acl      = "public-read"
 }
 
-# Upload files to bucket two
 resource "aws_s3_object" "bucket_two_files" {
   for_each = { for file in local.website_two_files : file => file }
   bucket   = aws_s3_bucket.static_website_two.bucket
   key      = each.value
   source   = "${path.module}/../../apps/web-site-two/${each.value}"
-  acl      = "public-read"
 }
